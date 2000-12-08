93abc9f50159f35ec83f1b3479c80c09dec890e9
kengo ichiki <kengoichiki@gmail.com>
2000-12-08 06:13:40 +0000

    started, based on ewald-3f.c: 2.1 2000/12/08 05:04:14 ichiki Exp

--- src/ewald-3ft.c
+++ src/ewald-3ft.c
@@ -1,11 +1,11 @@
 /* Beenakker's formulation of Ewald summation technique for RP tensor in 3D
  * Copyright (C) 1993-1996,1999-2000 Kengo Ichiki
  *               <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ewald-3ft.c,v 2.1 2000/12/08 05:04:14 ichiki Exp $
+ * $Id: ewald-3ft.c,v 2.2 2000/12/08 06:13:40 ichiki Exp $
  *
  * 3 dimensional hydrodynamics, 3D configuration
  * periodic boundary condition in 3 direction,
- * F version
+ * FT version
  * non-dimension formulation
  */
 #include <math.h>
@@ -18,17 +18,17 @@
 #include "cholesky.h" /* cholesky() */
 #endif /* ZETA */
 
-#include "ewald-3f.h"
+#include "ewald-3ft.h"
 
-/* calc ewald-summed mobility for F version
+/* calc ewald-summed mobility for FT version
  * INPUT
  *  n : # of all (mobile and fixed) particles
  *  x [n * 3] : position of particles
  * OUTPUT
- *  mat [n * 3 * n * 3] : mobility matrix
+ *  mat [n * 6 * n * 6] : mobility matrix
  */
 void
-calc_mob_ewald_3f (int n, double *x, double *mat)
+calc_mob_ewald_3ft (int n, double *x, double *mat)
 {
   extern int pcellx, pcelly, pcellz;
   extern int kmaxx, kmaxy, kmaxz;
@@ -43,8 +43,12 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
   clock_t ctmp1, ctmp2, ctmp3;
 #endif /* ZETA */
 
-  double m1i, m1n;
-  double m1xx, m1yy, m1zz, m1xy, m1yz, m1zx;
+  double a1, a2;
+  double axx, ayy, azz, axy, ayz, azx;
+  double b1;
+  double bx, by, bz;
+  double c1, c2;
+  double cxx, cyy, czz, cxy, cyz, czx;
   double ex, ey, ez;
   double exx, eyy, ezz, exy, eyz, ezx;
   double xx, yy, zz, rr;
@@ -52,26 +56,49 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
   double s, s2;
   double rlx, rly, rlz;
 
-  int n3;
+  int n6;
   int i, j;
-  int i1, i2, i3, j1, j2, j3;
+  int ix, iy, iz;
+  int jx, jy, jz;
+  int i1, i2, i3, i4, i5, i6;
+  int j1, j2, j3, j4, j5, j6;
   int m1, m2, m3;
 
   double k1, k2, k3, kk, k4z;
+  double k;
   double cfactor;
-  double mob, mob11, mob22, mob33, mob12, mob23, mob31;
+  double mob;
+  double pi8vol;
 
 
-  n3 = n * 3;
+  /* temporary setting : we should be modefy GLOBAL pi6vol */
+  pi8vol = pi6vol * 8.0 / 6.0; /* 8 pi / Vol */
+
+  n6 = n * 6;
   /* clear matrix */
-  for (i = 0; i < n3 * n3; i++)
+  for (i = 0; i < n6 * n6; i++)
     {
       mat [i] = 0.0;
     }
   /* diagonal part ( self part ) */
-  for (i = 0; i < n3; i++)
+  for (i = 0; i < n; i++)
     {
-      mat [i * n3 + i] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+      i1 = i * 6;
+      i2 = i1 + 1;
+      i3 = i1 + 2;
+      i4 = i1 + 3;
+      i5 = i1 + 4;
+      i6 = i1 + 5;
+
+      /* A part */
+      mat [i1 * n6 + i1] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+      mat [i2 * n6 + i2] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+      mat [i3 * n6 + i3] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+
+      /* C part */
+      mat [i4 * n6 + i4] = 0.75 - zaspi * 10.0 * za2;
+      mat [i5 * n6 + i5] = 0.75 - zaspi * 10.0 * za2;
+      mat [i6 * n6 + i6] = 0.75 - zaspi * 10.0 * za2;
     }
 
 #ifdef ZETA
@@ -81,14 +108,26 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
   /* first Ewald part ( real space ) */
   for (i = 0; i < n; i++)
     {
-      i1 = i * 3;
-      i2 = i * 3 + 1;
-      i3 = i * 3 + 2;
+      ix = i * 3;
+      iy = ix + 1;
+      iz = ix + 2;
+      i1 = i * 6;
+      i2 = i1 + 1;
+      i3 = i1 + 2;
+      i4 = i1 + 3;
+      i5 = i1 + 4;
+      i6 = i1 + 5;
       for (j = 0; j < n; j++)
 	{
-	  j1 = j * 3;
-	  j2 = j * 3 + 1;
-	  j3 = j * 3 + 2;
+	  jx = j * 3;
+	  jy = jx + 1;
+	  jz = jx + 2;
+	  j1 = j * 6;
+	  j2 = j1 + 1;
+	  j3 = j1 + 2;
+	  j4 = j1 + 3;
+	  j5 = j1 + 4;
+	  j6 = j1 + 5;
 
 	  for (m1 = - pcellx; m1 <= pcellx; m1++)
 	    {
@@ -100,9 +139,9 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
 		    {
 		      rlz = lz * (double) m3;
   
-		      xx = x [j1] - x [i1] + rlx;
-		      yy = x [j2] - x [i2] + rly;
-		      zz = x [j3] - x [i3] + rlz;
+		      xx = x [jx] - x [ix] + rlx;
+		      yy = x [jy] - x [iy] + rly;
+		      zz = x [jz] - x [iz] + rlz;
 		      rr = sqrt (xx * xx + yy * yy + zz * zz);
 
 		      if (rr > 0.0)
@@ -122,34 +161,93 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
 			  eyz = ey * ez;
 			  ezx = ez * ex;
 
-			  m1i = (0.75 + 0.5 / s2) / s * erfc (zr)
+			  a1 = (0.75 + 0.5 / s2) / s * erfc (zr)
 			    + ((1.0 + zr2 *
 				(14.0 + 4.0 * zr2 *
 				 (- 5.0 + zr2))) / s2
 			       - 4.5 + 3.0 * zr2) * zaspi * exp (- zr2);
-			  m1n = (0.75 - 1.5 / s2) / s * erfc (zr)
+			  a2 = (0.75 - 1.5 / s2) / s * erfc (zr)
 			    + ((- 3.0 + zr2 *
 				(- 2.0 + 4.0 * zr2 *
 				 (4.0 - zr2))) / s2
 			       + 1.5 - 3.0 * zr2) * zaspi * exp (- zr2);
 
-			  m1xx = (m1i + m1n * exx);
-			  m1yy = (m1i + m1n * eyy);
-			  m1zz = (m1i + m1n * ezz);
-			  m1xy = m1n * exy;
-			  m1yz = m1n * eyz;
-			  m1zx = m1n * ezx;
+			  b1 = - 0.75 / s2 * erfc (zr)
+			    - 1.5 * (1.0 + zr2 *
+				     (- 6.0 + zr2 *
+				      2.0)) / s * zaspi * exp (- zr2);
 
-			  mat[i1 * n3 + j1] += m1xx;
-			  mat[i2 * n3 + j2] += m1yy;
-			  mat[i3 * n3 + j3] += m1zz;
+			  c1 = - 3.0 / 8.0 / s2 / s * erfc (zr)
+			    - 0.75 * (1.0 + zr2 *
+				      (14.0 + zr2 *
+				       (-20.0 + zr2 *
+					4.0))) / s2 * zaspi * exp (- zr2);
+			  c2 = 9.0 / 8.0 / s2 / s * erfc (zr)
+			    - 0.75 * (- 3.0 + zr2 *
+				      (- 2.0 + zr2 *
+				       (16.0 + zr2 *
+					- 4.0))) / s2 * zaspi * exp (- zr2);
 
-			  mat[i1 * n3 + j2] += m1xy;
-			  mat[i2 * n3 + j1] += m1xy;
-			  mat[i2 * n3 + j3] += m1yz;
-			  mat[i3 * n3 + j2] += m1yz;
-			  mat[i3 * n3 + j1] += m1zx;
-			  mat[i1 * n3 + j3] += m1zx;
+			  axx = (a1 + a2 * exx);
+			  ayy = (a1 + a2 * eyy);
+			  azz = (a1 + a2 * ezz);
+			  axy = a2 * exy;
+			  ayz = a2 * eyz;
+			  azx = a2 * ezx;
+
+			  bx = b1 * ex;
+			  by = b1 * ey;
+			  bz = b1 * ez;
+
+			  cxx = (c1 + c2 * exx);
+			  cyy = (c1 + c2 * eyy);
+			  czz = (c1 + c2 * ezz);
+			  cxy = c2 * exy;
+			  cyz = c2 * eyz;
+			  czx = c2 * ezx;
+
+			  /* A part */
+			  mat [i1 * n6 + j1] += axx;
+			  mat [i2 * n6 + j2] += ayy;
+			  mat [i3 * n6 + j3] += azz;
+
+			  mat [i1 * n6 + j2] += axy;
+			  mat [i2 * n6 + j1] += axy;
+			  mat [i2 * n6 + j3] += ayz;
+			  mat [i3 * n6 + j2] += ayz;
+			  mat [i3 * n6 + j1] += azx;
+			  mat [i1 * n6 + j3] += azx;
+
+			  /* B part */
+			  /* note: r = x_i-x_j so that jj = 11 and ji = 12 */
+			  /* B21 = -B12*/
+			  mat [i4 * n6 + j2] += - bz;
+			  mat [i4 * n6 + j3] +=   by;
+			  mat [i5 * n6 + j1] +=   bz;
+			  mat [i5 * n6 + j3] += - bx;
+			  mat [i6 * n6 + j1] += - by;
+			  mat [i6 * n6 + j2] +=   bx;
+
+			  /* B_ part */
+			  /* BT21 = -B12 */
+			  mat [i1 * n6 + j5] += - bz;
+			  mat [i1 * n6 + j6] +=   by;
+			  mat [i2 * n6 + j4] +=   bz;
+			  mat [i2 * n6 + j6] += - bx;
+			  mat [i3 * n6 + j4] += - by;
+			  mat [i3 * n6 + j5] +=   bx;
+
+			  /* C part */
+			  mat [i4 * n6 + j4] += cxx;
+			  mat [i5 * n6 + j5] += cyy;
+			  mat [i6 * n6 + j6] += czz;
+
+			  mat [i4 * n6 + j5] += cxy;
+			  mat [i5 * n6 + j4] += cxy;
+			  mat [i5 * n6 + j6] += cyz;
+			  mat [i6 * n6 + j5] += cyz;
+			  mat [i6 * n6 + j4] += czx;
+			  mat [i4 * n6 + j6] += czx;
 			}
 		    }
 		}
@@ -174,48 +272,107 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
 	      if (m1 != 0 || m2 != 0 || m3 != 0)
 		{
 		  kk = k1 * k1 + k2 * k2 + k3 * k3;
+		  k = sqrt (kk);
 		  k4z = kk / 4.0 / zeta2;
-		  mob = pi6vol
-		    * (1.0 - kk / 3.0)
+		  mob = pi8vol
 		    * (1.0 + k4z * (1.0 + 2.0 * k4z))
 		    / kk * exp (- k4z);
 
-		  mob11 = (1.0 - k1 * k1 / kk) * mob;
-		  mob22 = (1.0 - k2 * k2 / kk) * mob;
-		  mob33 = (1.0 - k3 * k3 / kk) * mob;
-		  mob12 = - k1 * k2 / kk * mob;
-		  mob23 = - k2 * k3 / kk * mob;
-		  mob31 = - k3 * k1 / kk * mob;
+		  a1 = 0.75 * (1.0 - kk / 3.0) * mob;
+		  b1 = - 3.0 / 8.0 * k * mob;
+		  c1 = 3.0 / 16.0 * kk * mob;
+
+		  axx = (1.0 - k1 * k1 / kk) * a1;
+		  ayy = (1.0 - k2 * k2 / kk) * a1;
+		  azz = (1.0 - k3 * k3 / kk) * a1;
+		  axy = - k1 * k2 / kk * a1;
+		  ayz = - k2 * k3 / kk * a1;
+		  azx = - k3 * k1 / kk * a1;
+
+		  bx = b1 * k1 / k;
+		  by = b1 * k2 / k;
+		  bz = b1 * k3 / k;
+
+		  cxx = (1.0 - k1 * k1 / kk) * c1;
+		  cyy = (1.0 - k2 * k2 / kk) * c1;
+		  czz = (1.0 - k3 * k3 / kk) * c1;
+		  cxy = - k1 * k2 / kk * c1;
+		  cyz = - k2 * k3 / kk * c1;
+		  czx = - k3 * k1 / kk * c1;
 
 		  for (i = 0; i < n; i++)
 		    {
-		      i1 = i * 3;
-		      i2 = i * 3 + 1;
-		      i3 = i * 3 + 2;
+		      ix = i * 3;
+		      iy = ix + 1;
+		      iz = ix + 2;
+		      i1 = i * 6;
+		      i2 = i1 + 1;
+		      i3 = i1 + 2;
+		      i4 = i1 + 3;
+		      i5 = i1 + 4;
+		      i6 = i1 + 5;
 		      for (j = 0; j < n; j++)
 			{
-			  j1 = j * 3;
-			  j2 = j * 3 + 1;
-			  j3 = j * 3 + 2;
+			  jx = j * 3;
+			  jy = jx + 1;
+			  jz = jx + 2;
+			  j1 = j * 6;
+			  j2 = j1 + 1;
+			  j3 = j1 + 2;
+			  j4 = j1 + 3;
+			  j5 = j1 + 4;
+			  j6 = j1 + 5;
 
-			  xx = x [j1] - x [i1];
-			  yy = x [j2] - x [i2];
-			  zz = x [j3] - x [i3];
+			  xx = x [jx] - x [ix];
+			  yy = x [jy] - x [iy];
+			  zz = x [jz] - x [iz];
 
 			  cfactor = cos (+ k1 * xx
 					 + k2 * yy
 					 + k3 * zz);
 
-			  mat [i1 * n3 + j1] += cfactor * mob11;
-			  mat [i2 * n3 + j2] += cfactor * mob22;
-			  mat [i3 * n3 + j3] += cfactor * mob33;
+			  /* A part */
+			  mat [i1 * n6 + j1] += cfactor * axx;
+			  mat [i2 * n6 + j2] += cfactor * ayy;
+			  mat [i3 * n6 + j3] += cfactor * azz;
 
-			  mat [i1 * n3 + j2] += cfactor * mob12;
-			  mat [i2 * n3 + j1] += cfactor * mob12;
-			  mat [i2 * n3 + j3] += cfactor * mob23;
-			  mat [i3 * n3 + j2] += cfactor * mob23;
-			  mat [i3 * n3 + j1] += cfactor * mob31;
-			  mat [i1 * n3 + j3] += cfactor * mob31;
+			  mat [i1 * n6 + j2] += cfactor * axy;
+			  mat [i2 * n6 + j1] += cfactor * axy;
+			  mat [i2 * n6 + j3] += cfactor * ayz;
+			  mat [i3 * n6 + j2] += cfactor * ayz;
+			  mat [i3 * n6 + j1] += cfactor * azx;
+			  mat [i1 * n6 + j3] += cfactor * azx;
+
+			  /* B part */
+			  /* note: r = x_i-x_j so that jj = 11 and ji = 12 */
+			  /* B21 = -B12*/
+			  mat [i4 * n6 + j2] += - cfactor * bz;
+			  mat [i4 * n6 + j3] +=   cfactor * by;
+			  mat [i5 * n6 + j1] +=   cfactor * bz;
+			  mat [i5 * n6 + j3] += - cfactor * bx;
+			  mat [i6 * n6 + j1] += - cfactor * by;
+			  mat [i6 * n6 + j2] +=   cfactor * bx;
+
+			  /* B_ part */
+			  /* BT21 = -B12 */
+			  mat [i1 * n6 + j5] += - cfactor * bz;
+			  mat [i1 * n6 + j6] +=   cfactor * by;
+			  mat [i2 * n6 + j4] +=   cfactor * bz;
+			  mat [i2 * n6 + j6] += - cfactor * bx;
+			  mat [i3 * n6 + j4] += - cfactor * by;
+			  mat [i3 * n6 + j5] +=   cfactor * bx;
+
+			  /* C part */
+			  mat [i4 * n6 + j4] += cfactor * cxx;
+			  mat [i5 * n6 + j5] += cfactor * cyy;
+			  mat [i6 * n6 + j6] += cfactor * czz;
+
+			  mat [i4 * n6 + j5] += cfactor * cxy;
+			  mat [i5 * n6 + j4] += cfactor * cxy;
+			  mat [i5 * n6 + j6] += cfactor * cyz;
+			  mat [i6 * n6 + j5] += cfactor * cyz;
+			  mat [i6 * n6 + j4] += cfactor * czx;
+			  mat [i4 * n6 + j6] += cfactor * czx;
 			}
 		    }
 		}
@@ -230,6 +387,6 @@ calc_mob_ewald_3f (int n, double *x, double *mat)
   cpu2 = (double) (ctmp2 - ctmp1);
   cpu3 = (double) (ctmp3 - ctmp2);
 #else
-  cholesky (mat, n3);
+  cholesky (mat, n6);
 #endif /* ZETA */
 }
