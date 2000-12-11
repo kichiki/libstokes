80e737ae4a5aa7723a0079ddec9a60052825a6b3
kengo ichiki <kengoichiki@gmail.com>
2000-12-11 05:30:46 +0000

    (calc_mob_ewald_3fts): do not use pi6vol but pivol. use matrix_fts_ij() for self part, also. change name of variable 'mob' to 'kexp'. BUGS FIX on definition of xm, ym, zm in real space; this makes goot -- no zeta-dependence observed! (finally).

--- src/ewald-3fts.c
+++ src/ewald-3fts.c
@@ -1,7 +1,7 @@
 /* Beenakker's formulation of Ewald summation technique for RP tensor in 3D
  * Copyright (C) 1993-1996,1999-2000 Kengo Ichiki
  *               <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ewald-3fts.c,v 2.5 2000/12/09 10:11:17 ichiki Exp $
+ * $Id: ewald-3fts.c,v 2.6 2000/12/11 05:30:46 ichiki Exp $
  *
  * 3 dimensional hydrodynamics, 3D configuration
  * periodic boundary condition in 3 direction,
@@ -15,7 +15,7 @@
 #ifdef ZETA
 #include <time.h> /* clock() */
 /*#else
-#include "cholesky.h" /* cholesky() */
+  #include "cholesky.h"*/ /* cholesky() */
 #endif /* ZETA */
 
 #include "fts.h"
@@ -36,7 +36,8 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 
   extern double zeta, zeta2, zaspi, za2;
   extern double pi2;
-  extern double pi6vol;
+  /*extern double pi6vol;*/
+  extern double pivol; /* new global, hopefully replacement of pi6vol */
   extern double lx, ly, lz; /* cell size */
 
 #ifdef ZETA
@@ -62,21 +63,15 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
   int i, j;
   int ix, iy, iz;
   int jx, jy, jz;
-  int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11;
   int m1, m2, m3;
 
   double k1, k2, k3, kk, k4z;
   double k;
   double cf, sf;
-  double mob;
-  double pi8vol;
+  double kexp;
 
-  double mm5; /* for self part */
   double a2, c2;
 
-
-  /* temporary setting : we should be modefy GLOBAL pi6vol */
-  pi8vol = pi6vol * 8.0 / 6.0; /* 8 pi / Vol */
 
   n11 = n * 11;
   /* clear matrix */
@@ -84,39 +79,21 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
     mat [i] = 0.0;
 
   /* diagonal part ( self part ) */
+  xa = ya = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+  xc = yc = 0.75 - zaspi * za2 * 10.0;
+  xm = ym = zm = 0.9 - zaspi * za2 * (12.0 - 30.24 * za2);
+  /*xm = ym = zm = 0.9 - 3.0 * zaspi * za2 * (2.0 - 5.04 * za2);*/
   for (i = 0; i < n; i++)
     {
-      i1  = i * 11;
-      i2  = i1 + 1;
-      i3  = i1 + 2;
-      i4  = i1 + 3;
-      i5  = i1 + 4;
-      i6  = i1 + 5;
-      i7  = i1 + 6;
-      i8  = i1 + 7;
-      i9  = i1 + 8;
-      i10 = i1 + 9;
-      i11 = i1 + 10;
-
-      /* A part */
-      mat [i1 * n11 + i1] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
-      mat [i2 * n11 + i2] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
-      mat [i3 * n11 + i3] = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
-
-      /* C part */
-      mat [i4 * n11 + i4] = 0.75 - zaspi * 10.0 * za2;
-      mat [i5 * n11 + i5] = 0.75 - zaspi * 10.0 * za2;
-      mat [i6 * n11 + i6] = 0.75 - zaspi * 10.0 * za2;
-
-      /* M part */
-      mm5 = 9.0 / 20.0 - 0.3 * zaspi * za2 * (20.0 - 252.0 / 5.0 * za2);
-      mat [i7  * n11 + i7 ] = 4.0 / 3.0 * mm5; /* xx,xx */
-      mat [i7  * n11 + i11] = - 2.0 / 3.0 * mm5; /* xx,yy */
-      mat [i8  * n11 + i8 ] = mm5; /* xy,xy */
-      mat [i9  * n11 + i9 ] = mm5; /* xz,xz */
-      mat [i10 * n11 + i10] = mm5; /* yz,yz */
-      mat [i11 * n11 + i7 ] = - 2.0 / 3.0 * mm5; /* yy,xx */
-      mat [i11 * n11 + i11] = 4.0 / 3.0 * mm5; /* yy,yy */
+      matrix_fts_ij (i, i,
+		     0.0, 0.0, 0.0,
+		     xa, ya,
+		     0.0,
+		     xc, yc,
+		     0.0, 0.0,
+		     0.0,
+		     xm, ym, zm,
+		     n11, mat);
     }
 
 #ifdef ZETA
@@ -174,29 +151,32 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 			  xa = a2 + ya;
 	      
 			  yb = - 0.75 / s2 * erfc (zr)
-			    - 1.5 * (1.0 + zr2 *
+			    - 1.5 * (+ 1.0 + zr2 *
 				     (- 6.0 + zr2 *
-				      2.0)) / s * zaspi * exp (- zr2);
+				      (+ 2.0)))
+			    / s * zaspi * exp (- zr2);
 
 			  yc = - 3.0 / 8.0 / s2 / s * erfc (zr)
-			    - 0.75 * (1.0 + zr2 *
-				      (14.0 + zr2 *
+			    - 0.75 * (+ 1.0 + zr2 *
+				      (+ 14.0 + zr2 *
 				       (-20.0 + zr2 *
-					4.0))) / s2 * zaspi * exp (- zr2);
+					( + 4.0))))
+			    / s2 * zaspi * exp (- zr2);
 			  c2 = 9.0 / 8.0 / s2 / s * erfc (zr)
 			    - 0.75 * (- 3.0 + zr2 *
 				      (- 2.0 + zr2 *
-				       (16.0 + zr2 *
-					- 4.0))) / s2 * zaspi * exp (- zr2);
+				       (+ 16.0 + zr2 *
+					(- 4.0))))
+			    / s2 * zaspi * exp (- zr2);
 			  xc = c2 + yc;
 	      
 			  xg = (2.25 - 3.6 / s2) / s2 * erfc (zr)
 			    + (- 1.5 * (- 3.0 + zr2 *
-					6.0)
-			       - 0.8 * (9.0 + zr2 *
-					(6.0 + zr2 *
+					(+ 6.0))
+			       - 0.8 * (+ 9.0 + zr2 *
+					(+ 6.0 + zr2 *
 					 (- 48.0 + zr2 *
-					  12.0))) / s2)
+					  (+ 12.0)))) / s2)
 			    / s * zaspi * exp (- zr2);
 			  yg = 1.2 / s2 / s2 * erfc (zr)
 			    + (- 3.0 * ( zr2 *
@@ -217,32 +197,32 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 			    / s2 * zaspi * exp (- zr2);
 
 			  xm = (- 4.5 + 10.8 / s2) / s / s2 * erfc (zr)
-			    + (1.5 * (- 6.0 +  zr2 *
-				      (- 12.0 + zr2 *
-				       12.0))
-			       + 0.6 * (18.0 + zr2 *
-					(12.0 + zr2 *
-					 (30.0 + zr2 *
+			    + (+ 1.5 * (- 6.0 +  zr2 *
+					(- 12.0 + zr2 *
+					 (+ 12.0)))
+			       + 1.2 * (+ 18.0 + zr2 *
+					(+ 12.0 + zr2 *
+					 (+ 30.0 + zr2 *
 					  (- 66.0 + zr2 *
-					   12.0)))) / s2)
+					   (+ 12.0))))) / s2)
 			    / s2 * zaspi * exp (- zr2);
-			  ym = (4.25 - 7.2 / s2) / s / s2 * erfc (zr)
+			  ym = (+ 2.25 - 7.2 / s2) / s / s2 * erfc (zr)
 			    + (- 1.5 * (- 3.0 +  zr2 *
-					(6.0 + zr2 *
+					(+ 6.0 + zr2 *
 					 (- 12.0 + zr2 *
-					  4.0)))
-			       - 0.6 * (12.0 + zr2 *
-					(8.0 + zr2 *
+					  (+ 4.0))))
+			       - 1.2 * (+ 12.0 + zr2 *
+					(+ 8.0 + zr2 *
 					 (- 22.0 + zr2 *
-					  (58.0 + zr2 *
+					  (+ 58.0 + zr2 *
 					   (- 34.0 + zr2 *
-					    4.0))))) / s2)
+					    (+ 4.0)))))) / s2)
 			    / s2 * zaspi * exp (- zr2);
-			  zm = 4.5 / s2 / s / s2 * erfc (zr)
-			    + (- 1.5 * (0.0 +  zr2 *
-					(8.0 + zr2 *
+			  zm = + 1.8 / s2 / s / s2 * erfc (zr)
+			    + (- 1.5 * (+ 0.0 +  zr2 *
+					(+ 8.0 + zr2 *
 					 (- 4.0)))
-			       - 0.6 * (- 3.0 + zr2 *
+			       - 1.2 * (- 3.0 + zr2 *
 					(- 2.0 + zr2 *
 					 (- 26.0 + zr2 *
 					  (+ 26.0 + zr2 *
@@ -284,7 +264,7 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 		  kk = k1 * k1 + k2 * k2 + k3 * k3;
 		  k = sqrt (kk);
 		  k4z = kk / 4.0 / zeta2;
-		  mob = pi8vol
+		  kexp = pivol
 		    * (1.0 + k4z * (1.0 + 2.0 * k4z))
 		    / kk * exp (- k4z);
 
@@ -292,22 +272,12 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 		  ey = k2 / k;
 		  ez = k3 / k;
 
-		  xa = 0.0;
-		  ya = 0.75 * (1.0 - kk / 3.0) * mob;
-
-		  yb = 3.0 / 8.0 * k * mob;
-
-		  xc = 0.0;
-		  yc = 3.0 / 16.0 * kk * mob;
-
-		  xg = 0.0;
-		  yg = 3.0 / 8.0 * (1.0 - 4.0 / 15.0 * kk) * k * mob;
-
-		  yh = 3.0 / 16.0 * kk * mob;
-      
-		  xm = 0.0;
-		  ym = 3.0 / 8.0 * (1.0 - kk / 5.0) * kk * mob;
-		  zm = 0.0;
+		  ya = 6.0 * (1.0 - kk / 3.0) * kexp;
+		  yb = 3.0 * k * kexp;
+		  yc = 3.0 / 2.0 * kk * kexp;
+		  yg = 3.0 * (1.0 - 4.0 / 15.0 * kk) * k * kexp;
+		  yh = 3.0 / 2.0 * kk * kexp;
+		  ym = 3.0 * (1.0 - kk / 5.0) * kk * kexp;
       
 		  for (i = 0; i < n; i++)
 		    {
@@ -334,13 +304,12 @@ calc_mob_ewald_3fts (int n, double *x, double *mat)
 
 			  matrix_fts_ij (j, i,
 					 ex, ey, ez,
-					 cf * xa, cf * ya,
+					 0.0, cf * ya,
 					 sf * yb,
-					 cf * xc, cf * yc,
-					 sf * xg, sf * yg,
+					 0.0, cf * yc,
+					 0.0, sf * yg,
 					 cf * yh,
-					 cf * xm, cf * ym,
-					 cf * zm,
+					 0.0, cf * ym, 0.0,
 					 n11, mat);
 			}
 		    }
