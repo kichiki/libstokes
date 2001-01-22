690b9d2cfa9e466a721e936da05f53e17524923a
kengo ichiki <kengoichiki@gmail.com>
2001-01-22 02:59:43 +0000

    (matrix_ft_atimes): added for atimes-type routine (O(N^2) scheme).

--- src/ft.c
+++ src/ft.c
@@ -1,6 +1,6 @@
 /* subroutine for the procedure of FT version
  * Copyright (C) 2000 Kengo Ichiki <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ft.c,v 1.2 2000/12/11 06:37:37 ichiki Exp $
+ * $Id: ft.c,v 1.3 2001/01/22 02:59:43 ichiki Exp $
  */
 #include "ft.h"
 
@@ -114,4 +114,98 @@ matrix_ft_ij (int i, int j,
   mat [i6 * n6 + j5] += c2 * eyz;
   mat [i6 * n6 + j4] += c2 * exz;
   mat [i4 * n6 + j6] += c2 * exz;
+}
+
+/* ATIMES version (for O(N^2) scheme) of
+ * store matrix in FT format with scalar functions
+ * r := pos [alpha(i)] - pos [beta(j)]
+ * only m [alpha(i), beta(j)] is stored.
+ * INPUT
+ *   x [6] : related to 'i'
+ *   ex, ey, ez := (x[i] - x[j]) / r
+ *   xa, ya, ... : scalar functions
+ * OUTPUT
+ *   y [n6] : related to 'j'
+ */
+void
+matrix_ft_atimes (double *x, double *y,
+		  double ex, double ey, double ez,
+		  double xa, double ya,
+		  double yb,
+		  double xc, double yc)
+{
+  double a1, a2;
+  double b1;
+  double c1, c2;
+
+  double b1x, b1y, b1z;
+
+  double exx, eyy, ezz, exy, eyz, exz;
+
+
+  /* I DON'T KNOW WHY I SHOULD DO THE FOLLOWING */
+  ex = - ex;
+  ey = - ey;
+  ez = - ez;
+
+
+  a1 = ya;
+  a2 = xa - ya;
+
+  b1 = yb;
+
+  c1 = yc;
+  c2 = xc - yc;
+
+  exx = ex * ex;
+  eyy = ey * ey;
+  ezz = ez * ez;
+  exy = ex * ey;
+  eyz = ey * ez;
+  exz = ez * ex;
+
+  b1x = b1 * ex;
+  b1y = b1 * ey;
+  b1z = b1 * ez;
+
+  /* A part */
+  y [ 0] += x [ 0] * (a1 + a2 * exx);
+  y [ 1] += x [ 1] * (a1 + a2 * eyy);
+  y [ 2] += x [ 2] * (a1 + a2 * ezz);
+  
+  y [ 0] += x [ 1] * (a2 * exy);
+  y [ 1] += x [ 0] * (a2 * exy);
+  y [ 1] += x [ 2] * (a2 * eyz);
+  y [ 2] += x [ 1] * (a2 * eyz);
+  y [ 2] += x [ 0] * (a2 * exz);
+  y [ 0] += x [ 2] * (a2 * exz);
+
+  /* B part */
+  /* eps_ijk e_k */
+  y [ 3] += x [ 1] * (+ b1z ); /* x,y */
+  y [ 3] += x [ 2] * (- b1y ); /* x,z */
+  y [ 4] += x [ 0] * (- b1z ); /* y,x */
+  y [ 4] += x [ 2] * (+ b1x ); /* y,z */
+  y [ 5] += x [ 0] * (+ b1y ); /* z,x */
+  y [ 5] += x [ 1] * (- b1x ); /* z,y */
+
+  /* BT part */
+  y [ 1] -= x [ 3] * (+ b1z ); /* y,x */
+  y [ 2] -= x [ 3] * (- b1y ); /* z,x */
+  y [ 0] -= x [ 4] * (- b1z ); /* x,y */
+  y [ 2] -= x [ 4] * (+ b1x ); /* z,y */
+  y [ 0] -= x [ 5] * (+ b1y ); /* x,z */
+  y [ 1] -= x [ 5] * (- b1x ); /* y,z */
+
+  /* C part */
+  y [ 3] += x [ 3] * (c1 + c2 * exx);
+  y [ 4] += x [ 4] * (c1 + c2 * eyy);
+  y [ 5] += x [ 5] * (c1 + c2 * ezz);
+
+  y [ 3] += x [ 4] * (c2 * exy);
+  y [ 4] += x [ 3] * (c2 * exy);
+  y [ 4] += x [ 5] * (c2 * eyz);
+  y [ 5] += x [ 4] * (c2 * eyz);
+  y [ 5] += x [ 3] * (c2 * exz);
+  y [ 3] += x [ 5] * (c2 * exz);
 }
