c734148314ce2fb8738b95e1d0be66e57745ae26
kengo ichiki <kengoichiki@gmail.com>
2001-01-22 03:04:59 +0000

    (atimes_ewald_3ft): added for atimes-type routine (O(N^2) scheme).

--- src/ewald-3ft.c
+++ src/ewald-3ft.c
@@ -1,7 +1,7 @@
 /* Beenakker's formulation of Ewald summation technique for RP tensor in 3D
  * Copyright (C) 1993-1996,1999-2000 Kengo Ichiki
  *               <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ewald-3ft.c,v 2.3 2000/12/11 06:11:27 ichiki Exp $
+ * $Id: ewald-3ft.c,v 2.4 2001/01/22 03:04:59 ichiki Exp $
  *
  * 3 dimensional hydrodynamics, 3D configuration
  * periodic boundary condition in 3 direction,
@@ -257,5 +257,250 @@ calc_mob_ewald_3ft (int n, double *x, double *mat)
   cpu3 = (double) (ctmp3 - ctmp2);
   /*#else
     cholesky (mat, n6);*/
+#endif /* ZETA */
+}
+
+/* ATIMES version (for O(N^2) scheme) of
+ * calc ewald-summed mobility for FT version
+ * INPUT
+ *  (global) pos [] : position of particles
+ *  n := np * 6
+ *  x [n * 6] : FT
+ * OUTPUT
+ *  y [n * 6] : UO
+ */
+void
+atimes_ewald_3ft (int n, double *x, double *y)
+{
+  extern int pcellx, pcelly, pcellz;
+  extern int kmaxx, kmaxy, kmaxz;
+
+  extern double zeta, zeta2, zaspi, za2;
+  extern double pi2;
+  extern double pivol;
+  extern double lx, ly, lz; /* cell size */
+
+  extern double *pos;
+
+#ifdef ZETA
+  extern double cpu1, cpu2, cpu3;
+  clock_t ctmp1, ctmp2, ctmp3;
+#endif /* ZETA */
+
+  double xa, ya; 
+  double yb;
+  double xc, yc;
+
+  double ex, ey, ez;
+
+  double xx, yy, zz, rr;
+  double zr, zr2;
+  double s, s2;
+  double rlx, rly, rlz;
+
+  int np;
+  int i, j;
+  int i6, j6;
+  int ix, iy, iz;
+  int jx, jy, jz;
+  int m1, m2, m3;
+
+  double k1, k2, k3, kk, k4z;
+  double k;
+  double cf, sf;
+  double kexp;
+
+  double erfczr;
+  double expzr2;
+
+  double a2, c2;
+
+
+  np = n / 6;
+
+  /* clear result */
+  for (i = 0; i < n; i ++)
+    y [i] = 0.0;
+
+  /* diagonal part ( self part ) */
+  xa = ya = 1.0 - zaspi * (6.0 - 40.0 / 3.0 * za2);
+  xc = yc = 0.75 - zaspi * za2 * 10.0;
+  for (i = 0; i < np; i++)
+    {
+      i6 = i * 6;
+      matrix_ft_atimes (x + i6, y + i6,
+			 0.0, 0.0, 0.0,
+			 xa, ya,
+			 0.0,
+			 xc, yc);
+    }
+
+#ifdef ZETA
+  ctmp1 = clock ();
+#endif /* ZETA */
+
+  /* first Ewald part ( real space ) */
+  for (i = 0; i < np; i++)
+    {
+      i6 = i * 6;
+      ix = i * 3;
+      iy = ix + 1;
+      iz = ix + 2;
+      for (j = 0; j < np; j++)
+	{
+	  j6 = j * 6;
+	  jx = j * 3;
+	  jy = jx + 1;
+	  jz = jx + 2;
+
+	  for (m1 = - pcellx; m1 <= pcellx; m1++)
+	    {
+	      rlx = lx * (double) m1;
+	      for (m2 = - pcelly; m2 <= pcelly; m2++)
+		{
+		  rly = ly * (double) m2;
+		  for (m3 = - pcellz; m3 <= pcellz; m3++)
+		    {
+		      rlz = lz * (double) m3;
+  
+		      xx = pos [jx] - pos [ix] + rlx;
+		      yy = pos [jy] - pos [iy] + rly;
+		      zz = pos [jz] - pos [iz] + rlz;
+		      rr = sqrt (xx * xx + yy * yy + zz * zz);
+
+		      if (rr > 0.0)
+			{
+			  zr = zeta * rr;
+			  zr2 = zr * zr;
+			  s  = rr;
+			  s2 = s * s;
+
+			  erfczr = erfc (zr);
+			  expzr2 = zaspi * exp (- zr2);
+
+			  ex = xx / rr;
+			  ey = yy / rr;
+			  ez = zz / rr;
+
+			  ya = (0.75 + 0.5 / s2) / s * erfczr
+			    + ((1.0 + zr2 *
+				(14.0 + 4.0 * zr2 *
+				 (- 5.0 + zr2))) / s2
+			       - 4.5 + 3.0 * zr2)
+			    * expzr2;
+			  a2 = (0.75 - 1.5 / s2) / s * erfczr
+			    + ((- 3.0 + zr2 *
+				(- 2.0 + 4.0 * zr2 *
+				 (4.0 - zr2))) / s2
+			       + 1.5 - 3.0 * zr2)
+			    * expzr2;
+			  xa = a2 + ya;
+	      
+			  yb = - 0.75 / s2 * erfczr
+			    - 1.5 * (+ 1.0 + zr2 *
+				     (- 6.0 + zr2 *
+				      (+ 2.0)))
+			    / s * expzr2;
+
+			  yc = - 3.0 / 8.0 / s2 / s * erfczr
+			    - 0.75 * (+ 1.0 + zr2 *
+				      (+ 14.0 + zr2 *
+				       (-20.0 + zr2 *
+					( + 4.0))))
+			    / s2 * expzr2;
+			  c2 = 9.0 / 8.0 / s2 / s * erfczr
+			    - 0.75 * (- 3.0 + zr2 *
+				      (- 2.0 + zr2 *
+				       (+ 16.0 + zr2 *
+					(- 4.0))))
+			    / s2 * expzr2;
+			  xc = c2 + yc;
+	      
+			  matrix_ft_atimes (x + i6, y + j6,
+					     ex, ey, ez,
+					     xa, ya,
+					     yb,
+					     xc, yc);
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+#ifdef ZETA
+  ctmp2 = clock ();
+#endif /* ZETA */
+
+  /* Second Ewald part ( reciprocal space ) */
+  for (m1 = - kmaxx; m1 <= kmaxx; m1++)
+    {
+      k1 = pi2 * (double) m1 / lx;
+      for (m2 = - kmaxy; m2 <= kmaxy; m2++)
+	{
+	  k2 = pi2 * (double) m2 / ly;
+	  for (m3 = - kmaxz; m3 <= kmaxz; m3++)
+	    {
+	      k3 = pi2 * (double) m3 / lz;
+	      if (m1 != 0 || m2 != 0 || m3 != 0)
+		{
+		  kk = k1 * k1 + k2 * k2 + k3 * k3;
+		  k = sqrt (kk);
+		  k4z = kk / 4.0 / zeta2;
+		  kexp = pivol
+		    * (1.0 + k4z * (1.0 + 2.0 * k4z))
+		    / kk * exp (- k4z);
+
+		  ex = k1 / k;
+		  ey = k2 / k;
+		  ez = k3 / k;
+
+		  ya = 6.0 * (1.0 - kk / 3.0) * kexp;
+		  yb = 3.0 * k * kexp;
+		  yc = 3.0 / 2.0 * kk * kexp;
+      
+		  for (i = 0; i < np; i++)
+		    {
+		      i6 = i * 6;
+		      ix = i * 3;
+		      iy = ix + 1;
+		      iz = ix + 2;
+		      for (j = 0; j < np; j++)
+			{
+			  j6 = j * 6;
+			  jx = j * 3;
+			  jy = jx + 1;
+			  jz = jx + 2;
+
+			  xx = pos [jx] - pos [ix];
+			  yy = pos [jy] - pos [iy];
+			  zz = pos [jz] - pos [iz];
+
+			  cf = cos (+ k1 * xx
+				    + k2 * yy
+				    + k3 * zz);
+
+			  sf = - sin (+ k1 * xx
+				      + k2 * yy
+				      + k3 * zz);
+
+			  matrix_ft_atimes (x + i6, y + j6,
+					     ex, ey, ez,
+					     0.0, cf * ya,
+					     sf * yb,
+					     0.0, cf * yc);
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+#ifdef ZETA
+  ctmp3 = clock ();
+
+  cpu1 = (double) (ctmp3 - ctmp1);
+  cpu2 = (double) (ctmp2 - ctmp1);
+  cpu3 = (double) (ctmp3 - ctmp2);
 #endif /* ZETA */
 }
