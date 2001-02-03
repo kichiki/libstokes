2808bbe573b9efbafa54abc5ced557137998ac99
kengo ichiki <kengoichiki@gmail.com>
2001-02-03 13:29:43 +0000

    (calc_lub_3ft): added. (calc_lub_ft_2b): added.

--- src/ft.c
+++ src/ft.c
@@ -1,7 +1,12 @@
 /* subroutine for the procedure of FT version
  * Copyright (C) 2000-2001 Kengo Ichiki <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ft.c,v 1.6 2001/02/03 12:50:34 ichiki Exp $
+ * $Id: ft.c,v 1.7 2001/02/03 13:29:43 ichiki Exp $
  */
+#include <stdio.h> // fprintf ()
+#include <stdlib.h> // malloc ()
+#include <math.h> // sqrt ()
+#include "../FINITE/two-body-res.h" /* scalar_two_body_res () */
+
 #include "ft.h"
 
 /* ATIMES version (for O(N^2) scheme) of
@@ -153,12 +158,14 @@ set_ft_by_FT (int n,
     }
 }
 
-/*
+/* calc scalar functions of (M^inf)^-1 in FT
+ * INPUT
+ *   s : distance of particles
  * OUTPUT
  *  scalar_ft [10] :
  */
 void
-scalar_minv_FT (double s, double *scalar_ft)
+scalar_minv_ft (double s, double * scalar_ft)
 {
   double xa11, xa12;
   double ya11, ya12;
@@ -223,4 +230,131 @@ scalar_minv_FT (double s, double *scalar_ft)
   scalar_ft [7] = xc12;
   scalar_ft [8] = yc11;
   scalar_ft [9] = yc12;
+}
+
+/* calculate lubrication ft by uoe for all particles
+ * INPUT
+ *   (global) pos [np * 3] : position of particles
+ *   np : # particles
+ *   uo [np * 6] : velocity, angular velocity
+ * OUTPUT
+ *   ft [np * 6] : force, torque
+ */
+void
+calc_lub_3ft (int np, double * uo, double * ft)
+{
+  extern double * pos;
+
+  int i, j;
+  int i3, i6;
+  int j3, j6;
+
+
+  /* clear ft [np * 6] */
+  for (i = 0; i < np * 6; ++i)
+    ft [i] = 0.0;
+
+  for (i = 0; i < np; ++i)
+    {
+      i3 = i * 3;
+      i6 = i * 6;
+      for (j = i + 1; j < np; ++j)
+	{
+	  j3 = j * 3;
+	  j6 = j * 6;
+	  calc_lub_ft_2b (uo + i6, uo + j6,
+			  pos + i3, pos + j3,
+			  ft + i6, ft + j6);
+	  
+	}
+    }
+}
+
+/* calculate ft by uoe for pair of particles 1 and 2
+ * INPUT
+ *   (global) p : order of expansion
+ *   uo1 [6] : velocity, angular velocity, strain
+ *   uo2 [6] :
+ *   x1 [3] : position of particle 1
+ *   x2 [3] : position of particle 2
+ * OUTPUT
+ *   ft1 [6] : force, torque, stresslet
+ *   ft2 [6] :
+ */
+void
+calc_lub_ft_2b (double *uo1, double *uo2,
+		double *x1, double *x2,
+		double *ft1, double *ft2)
+{
+  double *res2b, *resinf;
+
+  double xx, yy, zz, rr;
+  double ex, ey, ez;
+
+  double xa11, ya11;
+  double xa12, ya12;
+  double yb11, yb12;
+  double xc11, yc11;
+  double xc12, yc12;
+
+
+  res2b = malloc (sizeof (double) * 44);
+  if (res2b == NULL)
+    {
+      fprintf (stderr, "allocation error in calc_lub_2b ().\n");
+      exit (1);
+    }
+  resinf = res2b + 22;
+
+  /* r := x[j] - x[i] for (j -> i) interaction */
+  xx = x2 [0] - x1 [0];
+  yy = x2 [1] - x1 [1];
+  zz = x2 [2] - x1 [2];
+  rr = sqrt (xx * xx + yy * yy + zz * zz);
+
+  if (rr <= 2.0)
+    rr = 2.0 + 1.0e-12;
+
+  ex = xx / rr;
+  ey = yy / rr;
+  ez = zz / rr;
+
+  /* calc scalar functions of lubrication */
+  scalar_two_body_res (rr, res2b);
+  scalar_minv_ft (rr, resinf);
+
+  xa11 = res2b [ 0] - resinf [ 0];
+  xa12 = res2b [ 1] - resinf [ 1];
+  ya11 = res2b [ 2] - resinf [ 2];
+  ya12 = res2b [ 3] - resinf [ 3];
+  yb11 = res2b [ 4] - resinf [ 4];
+  yb12 = res2b [ 5] - resinf [ 5];
+  xc11 = res2b [ 6] - resinf [ 6];
+  xc12 = res2b [ 7] - resinf [ 7];
+  yc11 = res2b [ 8] - resinf [ 8];
+  yc12 = res2b [ 9] - resinf [ 9];
+
+  matrix_ft_atimes (uo1, ft1,
+		    ex, ey, ez,
+		    xa11, ya11,
+		    yb11,
+		    xc11, yc11);
+  matrix_ft_atimes (uo2, ft1,
+		    ex, ey, ez,
+		    xa12, ya12,
+		    yb12,
+		    xc12, yc12);
+
+  matrix_ft_atimes (uo2, ft2,
+		    - ex, - ey, - ez,
+		    xa11, ya11,
+		    yb11,
+		    xc11, yc11);
+  matrix_ft_atimes (uo1, ft2,
+		    - ex, - ey, - ez,
+		    xa12, ya12,
+		    yb12,
+		    xc12, yc12);
+
+  free (res2b);
 }
