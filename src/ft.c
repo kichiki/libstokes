fefd70500569e09b7bfb97b0ee00c7732604b5c1
kengo ichiki <kengoichiki@gmail.com>
2001-02-03 12:50:34 +0000

    (scalar_minv_FT): added.

--- src/ft.c
+++ src/ft.c
@@ -1,6 +1,6 @@
 /* subroutine for the procedure of FT version
  * Copyright (C) 2000-2001 Kengo Ichiki <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: ft.c,v 1.5 2001/02/03 09:33:17 ichiki Exp $
+ * $Id: ft.c,v 1.6 2001/02/03 12:50:34 ichiki Exp $
  */
 #include "ft.h"
 
@@ -133,7 +133,7 @@ set_FT_by_ft (int n,
  */
 void
 set_ft_by_FT (int n,
-	      double *fts,
+	      double *ft,
 	      double *f, double *t)
 {
   int i;
@@ -151,4 +151,76 @@ set_ft_by_FT (int n,
 	  ft [i6 + 3 + j] = t [i3 + j];
 	}
     }
+}
+
+/*
+ * OUTPUT
+ *  scalar_ft [10] :
+ */
+void
+scalar_minv_FT (double s, double *scalar_ft)
+{
+  double xa11, xa12;
+  double ya11, ya12;
+  double yb11, yb12;
+  double xc11, xc12;
+  double yc11, yc12;
+
+  double s2, s3, s4, s6;
+  double dx, dy;
+
+
+  s2 = s * s;
+  s3 = s * s2;
+  s4 = s2 * s2;
+  s6 = s3 * s3;
+
+  dx = 4.0 * s6 - (3.0 * s2 - 2.0) * (3.0 * s2 - 2.0);
+  dy = 4.0 + s2 *
+    (- 12.0 + s2 *
+     (9.0 + s2 *
+      (- 32.0 + s2 *
+       (- 144.0 + s2 *
+	(- 36.0 + s2 *
+	 (64.0))))));
+
+  xa11 =
+    - 4.0 * s6
+    / dx;
+  xa12 =
+    + 2.0 * s3 * (3.0 * s2 - 2.0)
+    / dx;
+  ya11 =
+    - 16.0 * s6 * ((4.0 * s4 - 3.0) * s2 - 1.0)
+    / dy;
+  ya12 =
+    + 4.0 * s3 * (((12.0 * s2 + 8.0) * s4 + 3.0) * s2 - 2.0)
+    / dy;
+  yb11 =
+    + 16.0 * s6 * s * (3.0 * s2 + 4.0)
+    / dy;
+  yb12 =
+    - 8.0 * s4 * ((8.0 * s4 - 3.0) * s2 + 2.0)
+    / dy;
+  xc11 =
+    - 4.0 * s6 / 3.0 / (s6 - 1.0);
+  xc12 =
+    + 4.0 * s3 / 3.0 / (s6 - 1.0);
+  yc11 =
+    - 16.0 * s6 * (((16.0 * s2 - 9.0) * s2 - 24.0) * s2 - 4.0)
+    / 3.0 / dy;
+  yc12 =
+    - 8.0 * s3 * ((16.0 * s2 + 9.0) * s4 - 4.0)
+    / 3.0 / dy;
+
+  scalar_ft [0] = xa11;
+  scalar_ft [1] = xa12;
+  scalar_ft [2] = ya11;
+  scalar_ft [3] = ya12;
+  scalar_ft [4] = yb11;
+  scalar_ft [5] = yb12;
+  scalar_ft [6] = xc11;
+  scalar_ft [7] = xc12;
+  scalar_ft [8] = yc11;
+  scalar_ft [9] = yc12;
 }
