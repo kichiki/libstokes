aaa016ba06f44078cffe20f13655e69ea77717bc
kengo ichiki <kengoichiki@gmail.com>
2001-02-06 03:47:16 +0000

    (init_config_random): added, based on init_config_random_2d() in ewald-2.c. (check_overlap): copied from ewald-2.c.

--- src/stokes.c
+++ src/stokes.c
@@ -1,8 +1,9 @@
 /* utility routines for Ewald-summation code in 3D
  * Copyright (C) 2001 Kengo Ichiki <ichiki@kona.jinkan.kyoto-u.ac.jp>
- * $Id: stokes.c,v 1.5 2001/02/02 08:38:41 ichiki Exp $
+ * $Id: stokes.c,v 1.6 2001/02/06 03:47:16 ichiki Exp $
  */
 #include <math.h>
+#include <stdlib.h> // srand48(), drand48()
 
 #include "ewald-3.h"
 
@@ -15,6 +16,9 @@ double pivol;
 double lx, ly, lz; /* cell size */
 double llx [27], lly [27], llz [27]; /* for regist and lub */
 
+
+static int
+check_overlap (int np, double lx, double ly, double lz, double * pos);
 
 
 /* initialize parameters used in Ewald-summation code in 3D
@@ -229,4 +233,109 @@ init_config_FCC (double phi, int nx, int ny, int nz,
   (*lx) *= (double) nx;
   (*ly) *= (double) ny;
   (*lz) *= (double) nz;
+}
+
+/* initialize random configuration and the primary cell
+ * INPUT
+ *  seed : seed for random
+ *  phi : volume fraction of particles
+ *  np  : # particles
+ * OUTPUT
+ *  pos [np * 3] : positions of particles
+ *  lx, ly, lz   : geometry of the primary cell, where lz = 2.0 is fixed.
+ */
+void
+init_config_random (long seed,
+		    double phi, int np,
+		    double *pos, double *lx, double *ly, double *lz)
+{
+  int i;
+  int ix, iy, iz;
+  int i_tor = 1000;
+  int j;
+
+
+  srand48 (seed);
+
+  (*lx) = (*ly) = (*lz)
+    = pow (4.0 * M_PI * (double) np / phi / 3.0,
+	   1.0 / 3.0);
+
+  j = 0;
+  for (i = 0; i < np; i ++)
+    {
+    retry_init_config_random:
+      ix = i * 3;
+      iy = ix + 1;
+      iz = ix + 2;
+
+      pos [ix] = (*lx) * drand48 ();
+      pos [iy] = (*ly) * drand48 ();
+      pos [iz] = (*lz) * drand48 ();
+
+      if (check_overlap (i + 1, (*lx), (*ly), (*lz), pos) != 0)
+	{
+	  ++j;
+	  if (j > i_tor)
+	    {
+	      j = 0;
+	      if (i > 0)
+		--i;
+	    }
+	  goto retry_init_config_random;
+	}
+    }
+}
+
+/* check overlap
+ * INPUT
+ * OUTPUT (return value)
+ *  0 : no-overlap
+ *  1 : overlap
+ */
+static int
+check_overlap (int np, double lx, double ly, double lz, double * pos)
+{
+  int i;
+  int ix, iy, iz;
+  int j;
+  int jx, jy, jz;
+  int kx, ky, kz;
+
+  double rr;
+  double x, y, z;
+
+
+  for (i = 0; i < np; ++i)
+    {
+      ix = i * 3;
+      iy = ix + 1;
+      iz = ix + 2;
+      for (j = i + 1; j < np; ++j)
+	{
+	  jx = j * 3;
+	  jy = jx + 1;
+	  jz = jx + 2;
+	  for (kx = -1; kx <= 1; ++kx)
+	    {
+	      for (ky = -1; ky <= 1; ++ky)
+		{
+		  for (kz = -1; kz <= 1; ++kz)
+		    {
+		      x = pos [ix] - pos [jx] + (double) kx * lx;
+		      y = pos [iy] - pos [jy] + (double) ky * ly;
+		      z = pos [iz] - pos [jz] + (double) kz * lz;
+		      rr = x * x
+			+ y * y
+			+ z * z;
+		      if (rr <= 4.0)
+			{
+			  return 1;
+			}
+		    }
+		}
+	    }
+	}
+    }
+  return 0;
 }
