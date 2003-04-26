4dd42aa7435f264d8788c2d28b4176e8ff6bb5f2
kengo ichiki <kengoichiki@gmail.com>
2003-04-26 01:31:33 +0000

    Initial revision

---
+++ src/two-body-res.h
@@ -0,0 +1,8 @@
+/* header file for two-body-res.c --
+ * subroutines of exact 2-body resistance scalar functions
+ * Copyright (C) 1999,2003 Kengo Ichiki <ichiki@jhu.edu>
+ * $Id: two-body-res.h,v 1.1 2003/04/26 01:31:33 ichiki Exp $
+ */
+
+void
+scalar_two_body_res (double s, double *res);
