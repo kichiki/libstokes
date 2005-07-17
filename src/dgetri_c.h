986f62ae1a79cdb72fb394cc0153c47e80139688
kengo ichiki <kengoichiki@gmail.com>
2005-07-17 18:58:46 +0000

    add _DGETRI_C_H_ definition.

--- src/dgetri_c.h
+++ src/dgetri_c.h
@@ -1,8 +1,12 @@
 /* header file for dgetri.c --
  * C wrappers for LAPACK's dgetri_() and dgetrt_()
  * Copyright (C) 2005 Kengo Ichiki <kichiki@uwo.ca>
- * $Id: dgetri_c.h,v 1.1 2005/03/29 18:48:17 ichiki Exp $
+ * $Id: dgetri_c.h,v 1.2 2005/07/17 18:58:46 ichiki Exp $
  */
+#ifndef	_DGETRI_C_H_
+#define	_DGETRI_C_H_
 
 void lapack_inv (int n, const double *a,
 		 double *ai);
+
+#endif /* !_DGETRI_C_H_ */
