7fbcd9bf282923de2393274e7dd24c02d07a8ca4
kengo ichiki <kengoichiki@gmail.com>
2006-09-26 18:21:16 +0000

    minor style update.

--- src/bench.c
+++ src/bench.c
@@ -1,6 +1,6 @@
 /* routines to evaluate CPU time
- * Copyright (C) 1999,2002 Kengo Ichiki <ichiki@pegasus.me.jhu.edu>
- * $Id: bench.c,v 1.1 2002/05/27 22:31:14 ichiki Exp $
+ * Copyright (C) 1999-2006 Kengo Ichiki <kichiki@users.sourceforge.net>
+ * $Id: bench.c,v 1.2 2006/09/26 18:21:16 ichiki Exp $
  */
 
 #include <stdlib.h> /* exit() */
@@ -10,26 +10,28 @@
 
 #include "bench.h"
 
-/* benchmark routine */
-/*  現プロセスが消費した時間の<差分>をミリ秒単位で返す  */
+/* return process time difference since the last call in mili-seconds */
 long
 ptime_ms (void)
 {
-    struct rusage info;
-    static long t0 = 0;
-    long tmp = t0;
+  static long t0 = 0;
 
-    if (getrusage (RUSAGE_SELF, & info)) exit (1);
-    t0 = (info.ru_utime.tv_sec  + info.ru_stime.tv_sec)  * 1000
-       + (info.ru_utime.tv_usec + info.ru_stime.tv_usec) / 1000;
-    return (t0 - tmp);
+  struct rusage info;
+  long tmp = t0;
+
+  if (getrusage (RUSAGE_SELF, & info)) exit (1);
+  t0 = (info.ru_utime.tv_sec  + info.ru_stime.tv_sec)  * 1000
+    + (info.ru_utime.tv_usec + info.ru_stime.tv_usec) / 1000;
+  return (t0 - tmp);
 }
 
+/* return process time difference since the last call in mili-seconds */
 double
 ptime_ms_d (void)
 {
-  struct rusage info;
   static double t0 = 0;
+
+  struct rusage info;
   double tmp = t0;
 
   if (getrusage (RUSAGE_SELF, & info)) exit (1);
@@ -40,14 +42,16 @@ ptime_ms_d (void)
   return (t0 - tmp);
 }
 
+/* return process time difference since the last call in micro-seconds */
 long
 ptime_micros (void)
 {
-    struct rusage info;
-    static long t0 = 0;
-    long tmp = t0;
+  static long t0 = 0;
 
-    if (getrusage (RUSAGE_SELF, & info)) exit (1);
-    t0 = (info.ru_utime.tv_usec + info.ru_stime.tv_usec);
-    return (t0 - tmp);
+  struct rusage info;
+  long tmp = t0;
+
+  if (getrusage (RUSAGE_SELF, & info)) exit (1);
+  t0 = (info.ru_utime.tv_usec + info.ru_stime.tv_usec);
+  return (t0 - tmp);
 }
