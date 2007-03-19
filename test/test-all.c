c9e0492b93a1cbee463a0b50fcfcb9c71f70d697
kengo ichiki <kengoichiki@gmail.com>
2007-03-19 04:22:01 +0000

    added.

---
+++ test/test-all.c
@@ -0,0 +1,39 @@
+/* test code for libstokes
+ * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
+ * $Id: test-all.c,v 1.1 2007/03/19 04:22:01 kichiki Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include "check-lapack-inv.h" // check_lapack_inv_()
+#include "check-mul-matrices.h" // check_mul_matrices()
+#include "check-solve-gen-linear.h" // check_solve_gen_linear()
+
+#include "memory-check.h"
+
+
+/* main program */
+int
+main (int argc, char** argv)
+{
+
+  check_lapack_inv_ (100, 1);
+  check_split_merge (200, 200, 1);
+  check_mul_matrices (200, 1);
+  check_inverse_by_sub (100, 100, 1);
+
+  check_solve_gen_linear (200, 200, 1);
+
+  return 0;
+}
