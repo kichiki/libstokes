5cf2e2ca9aabb02cd2e2f119f887fb1162c776a6
kengo ichiki <kengoichiki@gmail.com>
2007-04-03 02:38:04 +0000

    add check_scalars_nonewald_poly().

--- test/test-all.c
+++ test/test-all.c
@@ -1,6 +1,6 @@
 /* test code for libstokes
  * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: test-all.c,v 1.1 2007/03/19 04:22:01 kichiki Exp $
+ * $Id: test-all.c,v 1.2 2007/04/03 02:38:04 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,6 +19,7 @@
 #include "check-lapack-inv.h" // check_lapack_inv_()
 #include "check-mul-matrices.h" // check_mul_matrices()
 #include "check-solve-gen-linear.h" // check_solve_gen_linear()
+#include "check-poly.h" // check_scalars_nonewald_poly()
 
 #include "memory-check.h"
 
@@ -34,6 +35,8 @@ main (int argc, char** argv)
   check_inverse_by_sub (100, 100, 1);
 
   check_solve_gen_linear (200, 200, 1);
+
+  check_scalars_nonewald_poly (1, 1.0e-10);
 
   return 0;
 }
