f8d44661b99e950d349e29a5fdb64f467a4e5b0e
kengo ichiki <kengoichiki@gmail.com>
2007-05-14 00:15:51 +0000

    added.

---
+++ src/ode-quaternion.h
@@ -0,0 +1,98 @@
+/* header file for ode-quaternion.c --
+ * ODE utility routines for angle by quaternion
+ * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
+ * $Id: ode-quaternion.h,v 1.1 2007/05/14 00:16:18 kichiki Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef	_ODE_QUATERNION_H_
+#define	_ODE_QUATERNION_H_
+
+
+void
+quaternion_W (const double *Q, double *W);
+
+void
+quaternion_Winv_lapack (const double *Q, double *W);
+
+void
+quaternion_Winv (const double *Q, double *W);
+
+void
+quaternion_by_Euler (double phi, double theta, double psi,
+		     double *Q);
+
+void
+quaternion_dQdt (const double *Q, const double *O,
+		 double *dQdt);
+
+/* calc dydt for center and angle by gsl_odeiv with hydrodynamics
+ * without inertia (zero Stokes number)
+ * INPUT
+ *  t   : current time
+ *  y[nm*3 + nm*4] : center position and quaternion of particles at time t
+ *  params : (struct ode_params*)ode.
+ *           the following parameters are used here;
+ *           ode->sys       : (struct stokes *)
+ *           ode->pos_fixed : 
+ *           ode->F [np*3]
+ *           ode->T [np*3]
+ *           ode->E [np*5]
+ *           ode->uf [np*3]
+ *           ode->of [np*3]
+ *           ode->ef [np*5]
+ *           ode->flag_mat
+ *           ode->flag_lub
+ *           ode->bonds     : (struct bonds *)
+ * OUTPUT
+ *  dydt[] := (d/dt) x(t) = U(t)
+ *         where U(t) := R^-1 . F, the terminal velocity
+ */
+int
+dydt_Q_hydro (double t, const double *y, double *dydt,
+	      void *params);
+
+/* calc dydt for center and angle by gsl_odeiv with hydrodynamics
+ * with inertia (scaled Stokes number)
+ * INPUT
+ *  t   : current time
+ *  y[nm*3 + nm*3 + nm*4] :
+ *           position, (real) velocity, and quaternion of particles at time t
+ *  params : (struct ode_params*)ode.
+ *           the following parameters are used here;
+ *           ode->sys       : (struct stokes *)
+ *           ode->pos_fixed : 
+ *           ode->F [np*3]
+ *           ode->T [np*3]
+ *           ode->E [np*5]
+ *           ode->uf [np*3]
+ *           ode->of [np*3]
+ *           ode->ef [np*5]
+ *           ode->flag_mat
+ *           ode->flag_lub
+ *           ode->stokes
+ *           ode->bonds     : (struct bonds *)
+ * OUTPUT
+ *  dydt[] := (d/dt) y(t), where y(t) = (x(t), U(t)).
+ *         (d/dt) x(t) = U(t)
+ *         (d/dt) U(t) = (1/stokes)(-U(t) + V(t)),
+ *         where V(t) := R^-1 . F, the terminal velocity
+ */
+int
+dydt_Q_hydro_st (double t, const double *y, double *dydt,
+		 void *params);
+
+
+#endif /* !_ODE_QUATERNION_H_ */
---
+++ test/check-ode-quaternion.c
@@ -0,0 +1,81 @@
+/* test code for ode-quaternion.c
+ * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
+ * $Id: check-ode-quaternion.c,v 1.1 2007/05/14 00:20:51 kichiki Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ode-quaternion.h>
+
+
+#include "check.h" // compare()
+
+
+/* check quaternion_Winv() with quaternion_Winv_lapack()
+ * INPUT
+ *  verbose : if non-zero, print results
+ *  tiny    : small number for check
+ * OUTPUT
+ *  (returned value) : 0 => passed
+ *                     otherwise => failed
+ */
+int
+check_quaternion_Winv (int verbose, double tiny)
+{
+  if (verbose != 0)
+    {
+      fprintf (stdout,
+	       "==================================================\n"
+	       "check_quaternion_Winv : start\n");
+    }
+
+  int check = 0;
+
+  /* this is not proper quaternion, but here
+   * it is to test the inversion
+   */
+  double Q[4];
+  Q[0] = 1.0;
+  Q[1] = 2.0;
+  Q[2] = 3.0;
+  Q[3] = 4.0;
+
+  double W1[16];
+  quaternion_Winv_lapack (Q, W1);
+
+
+  double W2[16];
+  quaternion_Winv (Q, W2);
+
+  int i;
+  for (i = 0; i < 16; i ++)
+    {
+      char label[80];
+      sprintf (label, " Winv[%d] :", i);
+      check += compare (W1[i], W2[i], label, verbose, tiny);
+    }
+
+  if (verbose != 0)
+    {
+      if (check == 0)
+	fprintf (stdout, " => PASSED\n\n");
+      else
+	fprintf (stdout, " => FAILED\n\n");
+    }
+
+  return (check);
+}
