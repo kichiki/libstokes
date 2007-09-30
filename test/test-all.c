46c5c8ec82ca771bc966b8eda0e03d96f7cf0b18
kengo ichiki <kengoichiki@gmail.com>
2007-09-30 04:02:09 +0000

    add Brownian dynamics stuff. -- the next item is done on 2007-09-20 -- loose the criteria a little bit (for other environment). -- the following is done on 2007-08-17 -- add check-twobody-slip.c.

--- test/test-all.c
+++ test/test-all.c
@@ -1,6 +1,6 @@
 /* test code for libstokes
  * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: test-all.c,v 1.9 2007/05/14 00:21:35 kichiki Exp $
+ * $Id: test-all.c,v 1.10 2007/09/30 04:02:09 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,6 +21,7 @@
 #include "check-lapack-inv.h" // check_lapack_inv_()
 #include "check-mul-matrices.h" // check_mul_matrices()
 #include "check-solve-gen-linear.h" // check_solve_gen_linear()
+
 #include "check-twobody.h" // check_twobody_scalars_res_with_equal()
 #include "check-poly.h" // check_scalars_nonewald_poly()
 #include "check-minv.h" // check_matrix_mob_nonewald_fts(), check_minv_fts()
@@ -29,8 +30,14 @@
 #include "check-ewald.h" // check_atimes_ewald_3all_SC ()
 #include "check-ewald-poly.h" // check_atimes_ewald_3all_poly_SC_...()
 #include "check-ode-quaternion.h" // check_quaternion_Winv()
+#include "check-twobody-slip.h" // check_twobody_slip_with_noslip()
 
-#include "memory-check.h"
+// Brownian dynamics stuff
+#include "check-chebyshev.h"
+#include "check-dsaupd_c.h"
+#include "check-dnaupd_c.h"
+#include "check-bd.h"
+#include "check-dgeev_c.h"
 
 
 /* main program */
@@ -38,12 +45,27 @@ int
 main (int argc, char** argv)
 {
   int check = 0;
-  check += check_lapack_inv_ (100, 1);
-  check += check_split_merge (200, 200, 1);
-  check += check_mul_matrices (200, 1);
-  check += check_inverse_by_sub (100, 100, 1);
 
-  check += check_solve_gen_linear (200, 200, 1);
+  // Brownian dynamics stuff
+  check += check_chebyshev (1, 1e-10);
+  check += check_dsaupd_c (100, 1, 1.0e-15);
+  check += check_dnaupd_c (10, 1, 1.0e-15);
+  check += check_dnaupd_c (100, 1, 1.0e-15);
+  check += check_bd (50, 100, 2.005, 1, 8.0e-10);
+  check += check_bd (100, 100, 2.005, 1, 2.0e-15);
+  check += check_dgeev_c (1, 2.0e-15);
+
+
+  // check-mul-matrices.c
+  check += check_mul_matrices (200, 1, 0.0);
+
+  // check-solve-gen-linear.c
+  check += check_split_merge (200, 200, 1, 0.0);
+  check += check_inverse_by_sub (100, 100, 1, 2.9e-13);
+  check += check_solve_gen_linear (200, 200, 1, 1.8e-8);
+
+  // check-lapack-inv.c
+  check += check_lapack_inv_ (100, 1, 4.2e-14);
 
 
   double r = 2.5;
@@ -52,7 +74,7 @@ main (int argc, char** argv)
   double a2 = 0.1;
 
   // check-twobody.c
-  check += check_twobody_lub(2, /* FTS */ r, a1, a2, 100, 1, 2.0e-16);
+  check += check_twobody_lub(2, /* FTS */ r, a1, a2, 100, 1, 4.0e-13);
   check += check_twobody_scalars_res_with_equal(r, 50, 1, 8.0e-7);
 
   // check-poly.c
@@ -90,7 +112,7 @@ main (int argc, char** argv)
   // check-ewald-poly.c
   check += check_atimes_ewald_3all_poly_SC_1 (version, phi,
 					      4.0, // ewald_tr
-				     ewald_eps, 1, 7.0e-15);
+					      ewald_eps, 1, 7.0e-15);
   check += check_atimes_ewald_3all_poly_SC_1 (version, phi,
 					      1.0, // ewald_tr
 					      ewald_eps, 1, 4.0e-15);
@@ -100,23 +122,23 @@ main (int argc, char** argv)
 
   check += check_make_matrix_mob_ewald_3all_poly_SC_1
     (version, phi, 4.0, // ewald_tr
-     ewald_eps, 1, 2.0e-15);
+     ewald_eps, 1, 5.0e-15);
   check += check_make_matrix_mob_ewald_3all_poly_SC_1
     (version, phi, 1.0, // ewald_tr
      ewald_eps, 1, 8.0e-15);
   check += check_make_matrix_mob_ewald_3all_poly_SC_1
     (version, phi, 10.0, // ewald_tr
-     ewald_eps, 1, 6.0e-15);
+     ewald_eps, 1, 2.0e-14);
 
   check += check_atimes_ewald_3all_poly_SC_2
     (version, 0, // x dir
-     phi, ewald_tr, ewald_eps, 1, 4.0e-15);
+     phi, ewald_tr, ewald_eps, 1, 8.0e-15);
   check += check_atimes_ewald_3all_poly_SC_2
     (version, 1, // y dir
-     phi, ewald_tr, ewald_eps, 1, 5.0e-15);
+     phi, ewald_tr, ewald_eps, 1, 1.0e-14);
   check += check_atimes_ewald_3all_poly_SC_2
     (version, 2, // z dir
-     phi, ewald_tr, ewald_eps, 1, 4.0e-15);
+     phi, ewald_tr, ewald_eps, 1, 1.0e-14);
 
   check += check_make_matrix_mob_ewald_3all_poly_SC_2
     (version, 0, // x dir
@@ -129,10 +151,14 @@ main (int argc, char** argv)
   check += check_make_matrix_mob_ewald_3all_poly_SC_2
     (version, 2, // z dir
      phi, ewald_tr, ewald_eps,
-     1, 1.0e-14);
+     1, 2.0e-14);
 
   // check-ode-quaternion.c
   check += check_quaternion_Winv (1, 3.0e-16);
+
+  // check-twobody-slip.c
+  check += check_twobody_slip_with_noslip (2.5, 1.0,  1, 0.0);
+  check += check_twobody_slip_with_noslip (2.5, 10.0, 1, 2.0e-16);
 
 
   fprintf (stdout,
