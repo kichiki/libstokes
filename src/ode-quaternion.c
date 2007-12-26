0fa9ac210bbabecd3f197dfeed725a41281d62eb
kengo ichiki <kengoichiki@gmail.com>
2007-12-26 06:35:19 +0000

    -- the following is done on 2007-12-24 -- (dydt_Q_hydro, dydt_Q_hydro_st): update stokes_set_shear_shift().

--- src/ode-quaternion.c
+++ src/ode-quaternion.c
@@ -1,6 +1,6 @@
 /* ODE utility routines for angle by quaternion
  * Copyright (C) 2007 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: ode-quaternion.c,v 1.4 2007/12/22 17:35:37 kichiki Exp $
+ * $Id: ode-quaternion.c,v 1.5 2007/12/26 06:35:19 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -190,7 +190,7 @@ dydt_Q_hydro (double t, const double *y, double *dydt,
 
   // set the current configuration into struct stokes "sys"
   stokes_set_pos_mobile (ode->sys, y);
-  stokes_set_shear_shift (ode->sys, t);
+  stokes_set_shear_shift (ode->sys, t, ode->t0, ode->s0);
 
   if (ode->bonds->n > 0)
     {
@@ -301,7 +301,7 @@ dydt_Q_hydro_st (double t, const double *y, double *dydt,
 
   // set the current configuration into struct stokes "sys"
   stokes_set_pos_mobile (ode->sys, y);
-  stokes_set_shear_shift (ode->sys, t);
+  stokes_set_shear_shift (ode->sys, t, ode->t0, ode->s0);
 
   if (ode->bonds->n > 0)
     {
