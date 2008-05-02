15461e0a1cc2add360e4b5ed0c0ac0fe7c946184
kengo ichiki <kengoichiki@gmail.com>
2008-05-02 03:43:28 +0000

    -- the following is done on 2007-04-30 -- (solve_mix_3all): correct the scaling from Particle-wise to Global.

--- src/ode.c
+++ src/ode.c
@@ -1,6 +1,6 @@
 /* ODE utility routines
  * Copyright (C) 2007-2008 Kengo Ichiki <kichiki@users.sourceforge.net>
- * $Id: ode.c,v 1.10 2008/04/29 03:29:56 kichiki Exp $
+ * $Id: ode.c,v 1.11 2008/05/02 03:43:28 kichiki Exp $
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -159,6 +159,386 @@ solve_mix_3all_HI (struct stokes *sys,
     }
 }
 
+/* wrapper for solving mob and mix problems in ODE routines
+ * for noHI version
+ */
+static void
+solve_mix_3all_noHI (struct stokes *sys,
+		     const double *fm, const double *tm, const double *em,
+		     const double *uf, const double *of, const double *ef,
+		     double *um, double *om, double *sm,
+		     double *ff, double *tf, double *sf)
+{
+  if (sys->version == 0)
+    {
+      // F version
+      solve_mix_3f_noHI
+	(sys,
+	 fm, uf,
+	 um, ff);
+    }
+  else if (sys->version == 1)
+    {
+      // FT version
+      solve_mix_3ft_noHI
+	(sys,
+	 fm, tm, uf, of,
+	 um, om, ff, tf);
+    }
+  else
+    {
+      // FTS version
+      solve_mix_3fts_noHI
+	(sys,
+	 fm, tm, em, uf, of, ef,
+	 um, om, sm, ff, tf, sf);
+    }
+}
+
+
+/**
+ ** conversion between Global scaling and Particle-wise scaling
+ **/
+static void
+scaling_G_to_P_fm (struct stokes *sys,
+		   const double *fm_G,
+		   double *fm_P)
+{
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      fm_P[ix] = fm_G[ix];
+      fm_P[iy] = fm_G[iy];
+      fm_P[iz] = fm_G[iz];
+    }
+}
+static void
+scaling_G_to_P_tm (struct stokes *sys,
+		   const double *tm_G,
+		   double *tm_P)
+{
+  if (sys->version <= 0)
+    {
+      // F version
+      return;
+    }
+
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      double a = sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      tm_P[ix] = tm_G[ix] / a;
+      tm_P[iy] = tm_G[iy] / a;
+      tm_P[iz] = tm_G[iz] / a;
+    }
+}
+static void
+scaling_G_to_P_em (struct stokes *sys,
+		   const double *em_G,
+		   double *em_P)
+{
+  if (sys->version <= 1)
+    {
+      // F or FT versions
+      return;
+    }
+
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      double a2 = sys->a[i] * sys->a[i];
+
+      int i1 = i*5;
+      int i2 = i1 + 1;
+      int i3 = i1 + 2;
+      int i4 = i1 + 3;
+      int i5 = i1 + 4;
+
+      em_P[i1] = em_G[i1] * a2;
+      em_P[i2] = em_G[i2] * a2;
+      em_P[i3] = em_G[i3] * a2;
+      em_P[i4] = em_G[i4] * a2;
+      em_P[i5] = em_G[i5] * a2;
+    }
+}
+static void
+scaling_G_to_P_uf (struct stokes *sys,
+		   const double *uf_G,
+		   double *uf_P)
+{
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      double a = sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      uf_P[ix] = uf_G[ix] * a;
+      uf_P[iy] = uf_G[iy] * a;
+      uf_P[iz] = uf_G[iz] * a;
+    }
+}
+static void
+scaling_G_to_P_of (struct stokes *sys,
+		   const double *of_G,
+		   double *of_P)
+{
+  if (sys->version <= 0)
+    {
+      // F version
+      return;
+    }
+
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      double a2 = sys->a[i] * sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      of_P[ix] = of_G[ix] * a2;
+      of_P[iy] = of_G[iy] * a2;
+      of_P[iz] = of_G[iz] * a2;
+    }
+}
+static void
+scaling_G_to_P_ef (struct stokes *sys,
+		   const double *ef_G,
+		   double *ef_P)
+{
+  if (sys->version <= 1)
+    {
+      // F or FT versions
+      return;
+    }
+
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      double a2 = sys->a[i] * sys->a[i];
+
+      int i1 = i*5;
+      int i2 = i1 + 1;
+      int i3 = i1 + 2;
+      int i4 = i1 + 3;
+      int i5 = i1 + 4;
+
+      ef_P[i1] = ef_G[i1] * a2;
+      ef_P[i2] = ef_G[i2] * a2;
+      ef_P[i3] = ef_G[i3] * a2;
+      ef_P[i4] = ef_G[i4] * a2;
+      ef_P[i5] = ef_G[i5] * a2;
+    }
+}
+
+/* convert Global (G) scaling to Particle-wise (P) scaling
+ */
+static void
+scaling_G_to_P (struct stokes *sys,
+		const double *fm_G,
+		const double *tm_G,
+		const double *em_G,
+		const double *uf_G,
+		const double *of_G,
+		const double *ef_G,
+		double *fm_P,
+		double *tm_P,
+		double *em_P,
+		double *uf_P,
+		double *of_P,
+		double *ef_P)
+{
+  scaling_G_to_P_fm (sys, fm_G, fm_P);
+  scaling_G_to_P_tm (sys, tm_G, tm_P);
+  scaling_G_to_P_em (sys, em_G, em_P);
+
+  scaling_G_to_P_uf (sys, uf_G, uf_P);
+  scaling_G_to_P_of (sys, of_G, of_P);
+  scaling_G_to_P_ef (sys, ef_G, ef_P);
+}
+
+static void
+scaling_P_to_G_um (struct stokes *sys,
+		   const double *um_P,
+		   double *um_G)
+{
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      double a = sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      um_G[ix] = um_P[ix] / a;
+      um_G[iy] = um_P[iy] / a;
+      um_G[iz] = um_P[iz] / a;
+    }
+}
+static void
+scaling_P_to_G_om (struct stokes *sys,
+		   const double *om_P,
+		   double *om_G)
+{
+  if (sys->version <= 0)
+    {
+      // F version
+      return;
+    }
+
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      double a2 = sys->a[i] * sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      om_G[ix] = om_P[ix] / a2;
+      om_G[iy] = om_P[iy] / a2;
+      om_G[iz] = om_P[iz] / a2;
+    }
+}
+static void
+scaling_P_to_G_sm (struct stokes *sys,
+		   const double *sm_P,
+		   double *sm_G)
+{
+  if (sys->version <= 1)
+    {
+      // F or FT versions
+      return;
+    }
+
+  int i;
+  for (i = 0; i < sys->nm; i ++)
+    {
+      double a = sys->a[i];
+
+      int i1 = i*5;
+      int i2 = i1 + 1;
+      int i3 = i1 + 2;
+      int i4 = i1 + 3;
+      int i5 = i1 + 4;
+
+      sm_G[i1] = sm_P[i1] * a;
+      sm_G[i2] = sm_P[i2] * a;
+      sm_G[i3] = sm_P[i3] * a;
+      sm_G[i4] = sm_P[i4] * a;
+      sm_G[i5] = sm_P[i5] * a;
+    }
+}
+static void
+scaling_P_to_G_ff (struct stokes *sys,
+		   const double *ff_P,
+		   double *ff_G)
+{
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      ff_G[ix] = ff_P[ix];
+      ff_G[iy] = ff_P[iy];
+      ff_G[iz] = ff_P[iz];
+    }
+}
+static void
+scaling_P_to_G_tf (struct stokes *sys,
+		   const double *tf_P,
+		   double *tf_G)
+{
+  if (sys->version <= 0)
+    {
+      // F version
+      return;
+    }
+
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      double a = sys->a[i];
+
+      int ix = i*3;
+      int iy = ix + 1;
+      int iz = ix + 2;
+
+      tf_G[ix] = tf_P[ix] * a;
+      tf_G[iy] = tf_P[iy] * a;
+      tf_G[iz] = tf_P[iz] * a;
+    }
+}
+static void
+scaling_P_to_G_sf (struct stokes *sys,
+		   const double *sf_P,
+		   double *sf_G)
+{
+  if (sys->version <= 1)
+    {
+      // F or FT versions
+      return;
+    }
+
+  int i;
+  for (i = sys->nm; i < sys->np; i ++)
+    {
+      double a = sys->a[i];
+
+      int i1 = i*5;
+      int i2 = i1 + 1;
+      int i3 = i1 + 2;
+      int i4 = i1 + 3;
+      int i5 = i1 + 4;
+
+      sf_G[i1] = sf_P[i1] * a;
+      sf_G[i2] = sf_P[i2] * a;
+      sf_G[i3] = sf_P[i3] * a;
+      sf_G[i4] = sf_P[i4] * a;
+      sf_G[i5] = sf_P[i5] * a;
+    }
+}
+/* convert Global (G) scaling to Particle-wise (P) scaling
+ */
+static void
+scaling_P_to_G (struct stokes *sys,
+		const double *um_P,
+		const double *om_P,
+		const double *sm_P,
+		const double *ff_P,
+		const double *tf_P,
+		const double *sf_P,
+		double *um_G,
+		double *om_G,
+		double *sm_G,
+		double *ff_G,
+		double *tf_G,
+		double *sf_G)
+{
+  scaling_P_to_G_um (sys, um_P, um_G);
+  scaling_P_to_G_om (sys, om_P, om_G);
+  scaling_P_to_G_sm (sys, sm_P, sm_G);
+
+  scaling_P_to_G_ff (sys, ff_P, ff_G);
+  scaling_P_to_G_tf (sys, tf_P, tf_G);
+  scaling_P_to_G_sf (sys, sf_P, sf_G);
+}
 
 /* wrapper for solving mob and mix problems in ODE routines
  * INPUT
@@ -185,42 +565,128 @@ solve_mix_3all (struct stokes *sys,
 		double *um, double *om, double *sm,
 		double *ff, double *tf, double *sf)
 {
-  if (flag_noHI == 0)
+  if (sys->a == NULL)
     {
-      // with HI
-      solve_mix_3all_HI (sys, flag_lub, flag_mat,
-			 fm, tm, em, uf, of, ef,
-			 um, om, sm, ff, tf, sf);
-    }
-  else
-    {
-      // without HI
-      if (sys->version == 0)
+      // monodisperse
+      if (flag_noHI == 0)
 	{
-	  // F version
-	  solve_mix_3f_noHI
-	    (sys,
-	     fm, uf,
-	     um, ff);
-	}
-      else if (sys->version == 1)
-	{
-	  // FT version
-	  solve_mix_3ft_noHI
-	    (sys,
-	     fm, tm, uf, of,
-	     um, om, ff, tf);
+	  // with HI
+	  solve_mix_3all_HI (sys, flag_lub, flag_mat,
+			     fm, tm, em, uf, of, ef,
+			     um, om, sm, ff, tf, sf);
 	}
       else
 	{
-	  // FTS version
-	  solve_mix_3fts_noHI
-	    (sys,
-	     fm, tm, em, uf, of, ef,
-	     um, om, sm, ff, tf, sf);
+	  // without HI
+	  solve_mix_3all_noHI (sys,
+			       fm, tm, em, uf, of, ef,
+			       um, om, sm, ff, tf, sf);
 	}
     }
+  else 
+    {
+      // polydisperse
+      int nm = sys->nm;
+      int nm3 = nm * 3;
+      int nm5 = nm * 5;
+      int nf = sys->np - nm;
+      int nf3 = nf * 3;
+      int nf5 = nf * 5;
 
+      // allocate memory for the Particle-wise scaling
+      double *fm_P = NULL;
+      double *tm_P = NULL;
+      double *em_P = NULL;
+      double *uf_P = NULL;
+      double *of_P = NULL;
+      double *ef_P = NULL;
+
+      double *um_P = NULL;
+      double *om_P = NULL;
+      double *sm_P = NULL;
+      double *ff_P = NULL;
+      double *tf_P = NULL;
+      double *sf_P = NULL;
+
+      // at least, F version
+      fm_P = (double *)malloc (sizeof (double) * nm3);
+      um_P = (double *)malloc (sizeof (double) * nm3);
+      CHECK_MALLOC (fm_P, "solve_mix_3all");
+      CHECK_MALLOC (um_P, "solve_mix_3all");
+      if (nf > 0)
+	{
+	  uf_P = (double *)malloc (sizeof (double) * nf3);
+	  ff_P = (double *)malloc (sizeof (double) * nf3);
+	  CHECK_MALLOC (uf_P, "solve_mix_3all");
+	  CHECK_MALLOC (ff_P, "solve_mix_3all");
+	}
+      if (sys->version > 0) // at least, FT version
+	{
+	  tm_P = (double *)malloc (sizeof (double) * nm3);
+	  om_P = (double *)malloc (sizeof (double) * nm3);
+	  CHECK_MALLOC (tm_P, "solve_mix_3all");
+	  CHECK_MALLOC (om_P, "solve_mix_3all");
+	  if (nf > 0)
+	    {
+	      of_P = (double *)malloc (sizeof (double) * nf3);
+	      tf_P = (double *)malloc (sizeof (double) * nf3);
+	      CHECK_MALLOC (of_P, "solve_mix_3all");
+	      CHECK_MALLOC (tf_P, "solve_mix_3all");
+	    }
+	  if (sys->version > 1) // FTS version
+	    {
+	      em_P = (double *)malloc (sizeof (double) * nm5);
+	      sm_P = (double *)malloc (sizeof (double) * nm5);
+	      CHECK_MALLOC (em_P, "solve_mix_3all");
+	      CHECK_MALLOC (sm_P, "solve_mix_3all");
+	      if (nf > 0)
+		{
+		  ef_P = (double *)malloc (sizeof (double) * nf5);
+		  sf_P = (double *)malloc (sizeof (double) * nf5);
+		  CHECK_MALLOC (ef_P, "solve_mix_3all");
+		  CHECK_MALLOC (sf_P, "solve_mix_3all");
+		}
+	    }
+	}
+
+      // convert the scaling from Global to Particle-wise
+      scaling_G_to_P (sys,
+		      fm, tm, em, uf, of, ef,
+		      fm_P, tm_P, em_P, uf_P, of_P, ef_P);
+
+      if (flag_noHI == 0)
+	{
+	  // with HI
+	  solve_mix_3all_HI (sys, flag_lub, flag_mat,
+			     fm_P, tm_P, em_P, uf_P, of_P, ef_P,
+			     um_P, om_P, sm_P, ff_P, tf_P, sf_P);
+	}
+      else
+	{
+	  solve_mix_3all_noHI (sys,
+			       fm_P, tm_P, em_P, uf_P, of_P, ef_P,
+			       um_P, om_P, sm_P, ff_P, tf_P, sf_P);
+	}
+
+      // convert the scaling from Particle-wise to Global
+      scaling_P_to_G (sys,
+		      um_P, om_P, sm_P, ff_P, tf_P, sf_P,
+		      um, om, sm, ff, tf, sf);
+
+      if (fm_P != NULL) free (fm_P);
+      if (tm_P != NULL) free (tm_P);
+      if (em_P != NULL) free (em_P);
+      if (uf_P != NULL) free (uf_P);
+      if (of_P != NULL) free (of_P);
+      if (ef_P != NULL) free (ef_P);
+
+      if (um_P != NULL) free (um_P);
+      if (om_P != NULL) free (om_P);
+      if (sm_P != NULL) free (sm_P);
+      if (ff_P != NULL) free (ff_P);
+      if (tf_P != NULL) free (tf_P);
+      if (sf_P != NULL) free (sf_P);
+    }
 }
 
 /* set the parameters to struct ode_params
